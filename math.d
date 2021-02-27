60ff51dd08db63bb6a1909334e3d182d664a3a16
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-02-27 13:32:10 +0100

    sin() function still not working

--- math.d
+++ math.d
@@ -11,15 +11,29 @@ int fact( int num ) {
 	return result;
 };
 
-int sin( int num ) {
-	// sine of an angle in degrees
-	
+float pow( float base, int exp ) {
+	float result = base;
+	for ( int i = 1; i < exp; i++ ) {
+		result = result * base;
+	}
+	return result;
 };
+
+
+float sin( int angle ) {
+	// sine of an angle in degrees
+	float radians = angle / 180;
+	float sine = radians - (pow(radians, 3) / fact(3)) + (pow(radians, 5) / fact(5)) - (pow(radians, 7) / fact(7)) + (pow(radians, 9) / fact(9));
+	return sine;
+};
+
 
 int main() {
 	// testing out the all functions
-	int num = fact(7);
-	writeln( num );
+	int num = fact( 7 );
+	float num2 = pow( 15.0, 2 );
+	float num3 = sin( 89 );
+	writeln( num3 );
 
 	return 0;
 }
