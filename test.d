29e50788614cdbbdf21bda975a5489d704c7eaec
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-03-06 12:55:46 +0100

    Moved the functions' testing to new file

---
+++ test.d
@@ -0,0 +1,14 @@
+import dalet.math;
+
+import std.stdio;
+
+int main() {
+	// testing out the all functions
+	int num1 = fact( 5 );
+	float num2 = pow( 15.0, 2 );
+	float num3 = sin( 80 );
+	int num4 = abs( 0 ); 
+	writeln( num1 );
+
+	return 0;
+}
