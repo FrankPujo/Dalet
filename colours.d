871e6c7af59a177edcf22e8b9aea38f37c4fa0f2
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-04-22 22:04:28 +0200

    Add all colours

--- colours.d
+++ colours.d
@@ -5,8 +5,32 @@ import std.stdio;
 string convertColour( string colour ) {
 	string colourCode;
 	switch ( colour ) {
+		case "black":
+			colourCode = "\033[30m";
+			break;
 		case "red":
 			colourCode = "\033[31m";
+			break;
+		case "green":
+			colourCode = "\033[32m";
+			break;
+		case "yellow":
+			colourCode = "\033[33m";
+			break;
+		case "blue":
+			colourCode = "\033[34m";
+			break;
+		case "magenta":
+			colourCode = "\033[35m";
+			break;
+		case "cyan":
+			colourCode = "\033[36m";
+			break;
+		case "white":
+			colourCode = "\033[37m";
+			break;
+		case "bold":
+			colourCode = "\033[1m";
 			break;
 		default:
 			colourCode = "";
